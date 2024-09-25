a6370ebd42b2f50dd0aeccb77b4f52f758c0af1b
SHISHIR GUPTA <shishirgupta6574@gmail.com>
2024-09-25 19:37:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 
 import pandas as pd
 import streamlit as st
-import pickle4 as pkl
+import pickle as pkl
 # from sklearn.preprocessing import StandardScaler
 
 # Load dataset (for feature range reference)
